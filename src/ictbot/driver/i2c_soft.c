237dcfa760b9731460433697d0c1a9751eaa52e8
myyerrol <myyerrol@126.com>
2026-02-01 13:22:13 +0800

    feat: add i2c soft driver

---
+++ src/ictbot/driver/i2c_soft.c
@@ -0,0 +1,81 @@
+#include "i2c_soft.h"
+
+void I2C_Soft_Init() {
+    printf("[i2c soft] init...\n");
+    GPIO_SetDir_Num(GPIO_0, I2C_SOFT_SCL_PIN, GPIO_OUT);
+    GPIO_SetDir_Num(GPIO_0, I2C_SOFT_SDA_PIN, GPIO_OUT);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 1);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SDA_PIN, 1);
+    printf("[i2c soft] init done!\n");
+}
+
+void I2C_Soft_Start() {
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SDA_PIN, 1);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 1);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SDA_PIN, 0);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 0);
+}
+
+void I2C_Soft_ReStart() {
+    GPIO_SetDir_Num(GPIO_0, I2C_SOFT_SDA_PIN, GPIO_OUT);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SDA_PIN, 1);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 1);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SDA_PIN, 0);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 0);
+}
+
+void I2C_Soft_Stop() {
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SDA_PIN, 0);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 1);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SDA_PIN, 1);
+}
+
+uint8_t I2C_Soft_WaitAck() {
+    uint8_t ack = 0;
+    
+    GPIO_SetDir_Num(GPIO_0, I2C_SOFT_SDA_PIN, GPIO_IN);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 1);
+
+    ack = GPIO_GetVal_Num(GPIO_0, I2C_SOFT_SDA_PIN);
+
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 0);
+    GPIO_SetDir_Num(GPIO_0, I2C_SOFT_SDA_PIN, GPIO_OUT);
+    
+    return ack == 0;
+}
+
+void I2C_Soft_SendAck(uint8_t ack) {
+    GPIO_SetDir_Num(GPIO_0, I2C_SOFT_SDA_PIN, GPIO_OUT);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SDA_PIN, ack ? 1 : 0);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 1);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 0);
+    GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SDA_PIN, 1);
+}
+
+uint8_t I2C_Soft_WriteByte(uint8_t byte) {
+    for (int i = 0; i < 8; i++) {
+        GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SDA_PIN, (byte & (0x80 >> i)) ? 1 : 0);
+        GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 1);
+        GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 0);
+    }
+    return I2C_Soft_WaitAck();
+}
+
+uint8_t I2C_Soft_ReadByteAck(uint8_t ack) {
+    uint8_t byte = 0;
+    GPIO_SetDir_Num(GPIO_0, I2C_SOFT_SDA_PIN, GPIO_IN);
+    for (int i = 0; i < 8; i++) {
+        GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 1);
+        byte <<= 1;
+        if (GPIO_GetVal_Num(GPIO_0, I2C_SOFT_SDA_PIN)) {
+            byte |= 0x01;
+        }
+        GPIO_SetVal_Num(GPIO_0, I2C_SOFT_SCL_PIN, 0);
+    }
+    I2C_Soft_SendAck(ack);
+    return byte;
+}
+
+uint8_t I2C_Soft_ReadByte() {
+    return I2C_Soft_ReadByteAck(1);
+}
---
+++ src/ictbot/driver/i2c_soft.h
@@ -0,0 +1,21 @@
+#ifndef I2C_SOFT_H
+#define I2C_SOFT_H
+
+#include <klib.h>
+#include <ecos.h>
+#include "timer.h"
+
+#define I2C_SOFT_SCL_PIN 14
+#define I2C_SOFT_SDA_PIN 15
+
+void I2C_Soft_Init();
+void I2C_Soft_Start();
+void I2C_Soft_ReStart();
+void I2C_Soft_Stop();
+uint8_t I2C_Soft_WriteByte(uint8_t byte);
+uint8_t I2C_Soft_WaitAck();
+void I2C_Soft_SendAck(uint8_t ack);
+uint8_t I2C_Soft_ReadByteAck(uint8_t ack);
+uint8_t I2C_Soft_ReadByte();
+
+#endif
